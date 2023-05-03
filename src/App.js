ef4e13f4b245bcb73293f38ebb69ac6ffc4c1f8e
Lucrecia-Miatello <128558301+Lucrecia-Miatello@users.noreply.github.com>
2023-05-03 20:32:46 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Divider, Input } from 'antd';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+    <Divider>
+      <form onSubmit={presion}>
+        <p>
+          <Input type="letter" name="frase1" />
+        </p>
+        <p>
+          <Input type="submit" value="Evaluar" />
+        </p>
+      </form>
+	  </Divider>
+     );
+
+
+    function presion(e) { 
+      e.preventDefault();
+      let palabra=(e.target.frase1.value)
+      let vocales = ['a', 'e', 'i', 'o', 'u'];
+      let contador = 0;
+      
+      for(let i=0; i < palabra.length; i++){
+        for(let l=0; l < vocales.length; l++){
+
+          if(palabra[i] == vocales[l]){contador++;}
+        }
+
+      }
+      alert(' vocales encontradas: ' +contador);
+    }
+  }
+  export default App;
